62ea8357cb6f8391aa46e9262a165dbd69c2a6d0
Sayan Ghosh Dastidar <sayangd.41@gmail.com>
2025-01-09 10:47:58 +0530

    HashMap replacing string request body

--- src/test/java/Tests/LibraryAPI.java
+++ src/test/java/Tests/LibraryAPI.java
@@ -1,25 +1,25 @@
 package tests;
 
+import java.util.HashMap;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
 
-public class AddBookTest
+public class LibraryAPI
 {
 	@Test
 	public void addBook()
 	{
+		HashMap <String, Object> jsonBody=new HashMap<>();
+		jsonBody.put("name", "Learn Appium Automation with Java");
+		jsonBody.put("isbn", "abcd");
+		jsonBody.put("aisle", "41414141");
+		jsonBody.put("author", "Sayan GD");
+
 		RestAssured.baseURI="http://216.10.245.166";
-		String addBookResponse=given().log().all().contentType(ContentType.JSON).body("{\r\n"
-				+ "\r\n"
-				+ "\"name\":\"Learn Appium Automation with Java\",\r\n"
-				+ "\"isbn\":\"abcd\",\r\n"
-				+ "\"aisle\":\"414141\",\r\n"
-				+ "\"author\":\"Sayan GD\"\r\n"
-				+ "}\r\n"
-				+ "")
+		String addBookResponse=given().log().all().contentType(ContentType.JSON).body(jsonBody)
 		.when().post("Library/Addbook.php")
 		.then().log().all().assertThat().statusCode(200).extract().response().asString();
 		JsonPath js=new JsonPath(addBookResponse);
