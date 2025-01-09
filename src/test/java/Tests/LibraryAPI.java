17ada785498257a14b9edc58ef10a650e974a3b9
Sayan Ghosh Dastidar <sayangd.41@gmail.com>
2025-01-09 11:13:56 +0530

    Reading data from Excel through Hashmap

--- src/test/java/Tests/LibraryAPI.java
+++ src/test/java/Tests/LibraryAPI.java
@@ -1,22 +1,28 @@
 package tests;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
+import utilities.ReadDataFromExcel;
 
 public class LibraryAPI
 {
 	@Test
-	public void addBook()
+	public void addBook() throws IOException
 	{
+		ReadDataFromExcel readData=new ReadDataFromExcel();
+		ArrayList<String> al=readData.readExcelData("Library API", "Add Book");
+
 		HashMap <String, Object> jsonBody=new HashMap<>();
-		jsonBody.put("name", "Learn Appium Automation with Java");
-		jsonBody.put("isbn", "abcd");
-		jsonBody.put("aisle", "41414141");
-		jsonBody.put("author", "Sayan GD");
+		jsonBody.put("name", al.get(1));
+		jsonBody.put("isbn", al.get(2));
+		jsonBody.put("aisle", al.get(3));
+		jsonBody.put("author", al.get(4));
 
 		RestAssured.baseURI="http://216.10.245.166";
 		String addBookResponse=given().log().all().contentType(ContentType.JSON).body(jsonBody)
